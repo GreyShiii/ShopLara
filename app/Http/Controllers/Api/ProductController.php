fda2e3213198e578c1dc823cb64e670ae4f6382d
ShiiGrey <xandergrey2531@gmail.com>
2026-07-21 22:44:56 +0800

    added api but not finished because laptop is lowbat

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreProductRequest as ApiStoreProductRequest;
+use App\Http\Requests\Api\UpdateProductRequest as ApiUpdateProductRequest;
+use App\Http\Requests\Product\StoreProductRequest;
+use App\Http\Requests\Product\UpdateProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $product = Product::simplePaginate(10);
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $product = Product::create($request->validated());
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product = $product->update($request->validated());
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product = $product->delete();
+        return response()->json($product, 204);
+    }
+}
